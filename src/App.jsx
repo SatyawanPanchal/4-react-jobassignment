44fb25c93669462d72cdf57585e27695e03a3f9c
SatyawanPanchal <panchal.satyawan@gmail.com>
2024-06-21 16:48:16 +0530

    responsive reset

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Menu from './Menu'
 import JobsCurrently from './JobsCurrently'
 import Duties from "./Duties";
 import './style.css'
+import Title from "./Title";
 
 const url = 'https://www.course-api.com/react-tabs-project';
 
@@ -10,36 +11,40 @@ const App = () => {
 
 
 
-  const [jobs,setJobs]=useState([])
-  const[loading,setLoading]=useState(true)
-  const [currentItem,setCurrentItem]=useState(1)
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [currentItem, setCurrentItem] = useState(1)
 
-  useEffect( ()=> {
-    async function  fetchJobs()
-    {
-      const response= await fetch(url);
-      const newJobs= await response.json();
-      setLoading(false)     
+  useEffect(() => {
+    async function fetchJobs() {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setLoading(false)
       setJobs(newJobs);
-    };   
+    };
     fetchJobs();
-  },[])
-  console.log("item",currentItem);
+  }, [])
+  console.log("item", currentItem);
 
 
-  if(loading)
-    {
-      return<div>Loading...</div>
-    }else{
-  return<>
-  <div className="title">
-  <h1>Satyawan App (Jobs company wise)</h1>
-  </div>
-  
-<Menu jobs={jobs} setCurrentItem={setCurrentItem}/>
-<JobsCurrently jobs={jobs} currentItem={currentItem}/>
-  
-  
-  </>;
-};}
+  if (loading) {
+    return <div>Loading...</div>
+  } else {
+    return <>
+    <div className="main_container">
+
+    
+    <Title/>
+
+      <div className="menudiv">
+      <Menu jobs={jobs} setCurrentItem={setCurrentItem} />
+      </div>
+      <div className="jobdiv">
+      <JobsCurrently jobs={jobs} currentItem={currentItem} />
+      </div>
+      </div>
+
+    </>;
+  };
+}
 export default App;
