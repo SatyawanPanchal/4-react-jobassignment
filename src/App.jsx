36812b2667106d274cc4536715c44d33b0338f78
SatyawanPanchal <panchal.satyawan@gmail.com>
2024-06-22 12:10:15 +0530

    resposive and sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -31,16 +31,19 @@ const App = () => {
     return <div>Loading...</div>
   } else {
     return <>
-    <div className="main_container">
 
-    
-    <Title/>
+      <div className="main_container">
 
-      <div className="menudiv">
-      <Menu jobs={jobs} setCurrentItem={setCurrentItem} />
-      </div>
-      <div className="jobdiv">
-      <JobsCurrently jobs={jobs} currentItem={currentItem} />
+
+        <Title />
+        <div className="app_container"> 
+
+        <div className="menudiv">
+          <Menu jobs={jobs} setCurrentItem={setCurrentItem} />
+        </div>
+        <div className="jobdiv">
+          <JobsCurrently jobs={jobs} currentItem={currentItem} />
+        </div>
       </div>
       </div>
 
